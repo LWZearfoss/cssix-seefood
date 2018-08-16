ff838948742949e86575b742182dc6b90dcbfc26
Luke Zearfoss <luke@Lukes-MacBook-Pro.local>
2018-08-15 18:26:14 -0700

    JS Rework of Project

--- main.py
+++ main.py
@@ -1,59 +1,14 @@
-import argparse
-import cloudstorage
-import google.cloud
-import io
 from flask import Flask, redirect, render_template, request, url_for
-from google.appengine.ext import blobstore, ndb
-from google.appengine.api import app_identity, images
 
 app = Flask(__name__)
-
-class Image(ndb.Model):
-    image_url = ndb.StringProperty()
-    image_blob = ndb.BlobKeyProperty()
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_form():
     if request.method == 'GET':
         return render_template('form.html')
     else:
-        image_file = request.files['file']
-        image_data = image_file.stream.read()
+        return redirect(url_for('map'))
 
-        filename = '/' + app_identity.get_default_gcs_bucket_name() + '/' + \
-            image_file.filename
-
-        with cloudstorage.open(
-            filename, mode='w', content_type=image_file.mimetype,
-            retry_params=cloudstorage.RetryParams(backoff_factor=1.1)
-        ) as gcs:
-            gcs.write(image_data)
-
-        blob_key = blobstore.create_gs_key('/gs' + filename)
-        image_url = images.get_serving_url(blob_key=blob_key)
-
-        image = Image(
-            image_url=image_url,
-            image_blob=blobstore.BlobKey(blob_key),
-        )
-        image.put()
-
-        return redirect(url_for('map', id=image.key.urlsafe()))
-
-def detect_web_uri(uri):
-    client = vision.ImageAnnotatorClient()
-    image = vision.types.Image()
-    image.source.image_uri = uri
-
-    response = client.web_detection(image=image)
-    annotations = response.web_detection
-
-    if annotations.web_entities:
-        query = annotations.web_entities[0].score
-        return query
-
-@app.route('/map/<id>')
-def map(id):
-    image_key = ndb.Key(urlsafe=id)
-    image = image_key.get()
+@app.route('/map', methods=['GET'])
+def map():
     return render_template('map.html')
---
+++ static/main.js
@@ -0,0 +1,63 @@
+
+'use strict';
+
+var CV_URL = 'https://vision.googleapis.com/v1/images:annotate?key=' + window.apiKey;
+
+$(function () {
+  $('#fileform').on('submit', uploadFiles);
+});
+
+/**
+ * 'submit' event handler - reads the image bytes and sends it to the Cloud
+ * Vision API.
+ */
+function uploadFiles (event) {
+  event.preventDefault(); // Prevent the default form post
+
+  // Grab the file and asynchronously convert to base64.
+  var file = $('#fileform [name=fileField]')[0].files[0];
+  var reader = new FileReader();
+  reader.onloadend = processFile;
+  reader.readAsDataURL(file);
+}
+
+/**
+ * Event handler for a file's data url - extract the image data and pass it off.
+ */
+function processFile (event) {
+  var content = event.target.result;
+  sendFileToCloudVision(content.replace('data:image/jpeg;base64,', ''));
+}
+
+/**
+ * Sends the given file contents to the Cloud Vision API and outputs the
+ * results.
+ */
+function sendFileToCloudVision (content) {
+
+  // Strip out the file prefix when you convert to json.
+  var request = {
+    requests: [{
+      image: {
+        content: content
+      },
+      features: [{
+        type: "WEB_DETECTION",
+        maxResults: 200
+      }]
+    }]
+  };
+
+  $.post({
+    url: CV_URL,
+    data: JSON.stringify(request),
+    contentType: 'application/json'
+  }).fail(function (jqXHR, textStatus, errorThrown) {
+    $('#results').text('ERRORS: ' + textStatus + ' ' + errorThrown);
+  }).done(push);
+}
+
+function push(data) {
+  data: JSON.stringify(data),
+      window.location = "/map"+"?q="+data.responses[0].webDetection.webEntities[0].description.split(' ').join('+').toLowerCase();
+}
